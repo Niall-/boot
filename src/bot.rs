6b68659d075f9f6b83c684fa96547a40b7aa4f74
Niall Coates <1349685+Niall-@users.noreply.github.com>
2021-06-25 04:16:39 +0100

    Initial support for parsing titles

--- src/bot.rs
+++ src/bot.rs
@@ -1,4 +1,6 @@
 use irc::client::prelude::*;
+use linkify::{Link, LinkFinder, LinkKind};
+use webpage::{Webpage, WebpageOptions};
 
 #[derive(Debug)]
 struct Msg<'a> {
@@ -28,10 +30,13 @@ pub async fn process_message(client: &Client, message: &Message) {
     let target = message.response_target();
 
     match &message.command {
-        Command::PRIVMSG(_target, message) => privmsg(
-            &client,
-            Msg::new(our_nick, source.unwrap(), target.unwrap(), message),
-        ).await,
+        Command::PRIVMSG(_target, message) => {
+            privmsg(
+                &client,
+                Msg::new(our_nick, source.unwrap(), target.unwrap(), message),
+            )
+            .await
+        }
         Command::KICK(channel, user, _text) => {
             kick(&client, Msg::new(our_nick, source.unwrap(), user, channel)).await
         }
@@ -43,10 +48,17 @@ pub async fn process_message(client: &Client, message: &Message) {
 }
 
 async fn privmsg(client: &Client, msg: Msg<'_>) {
+    if msg.target.starts_with("#") {
+        let mut finder = LinkFinder::new();
+        finder.kinds(&[LinkKind::Url]);
+        let links: Vec<_> = finder.links(&msg.content).collect();
+        process_titles(&client, &msg, links).await;
+    }
+
+    // past this point we only care about interactions with the bot
     let mut tokens = msg.content.split_whitespace();
     let next = tokens.next();
 
-    // past this point we only care about interactions with the bot
     match next {
         Some(n) if !n.starts_with(&msg.our_nick.to_lowercase()) => return,
         _ => (),
@@ -61,6 +73,41 @@ async fn privmsg(client: &Client, msg: Msg<'_>) {
     }
 }
 
-async fn kick(client: &Client, msg: Msg<'_>) {}
+async fn process_titles(client: &Client, msg: &Msg<'_>, links: Vec<Link<'_>>) {
+    let urls = links.into_iter().map(|x| x.as_str().to_string());
 
-async fn invite(client: &Client, msg: Msg<'_>) {}
+    // the following is adapted from
+    // https://stackoverflow.com/questions/63434977/how-can-i-spawn-asynchronous-methods-in-a-loop
+    let tasks: Vec<_> = urls
+        .into_iter()
+        .map(|l| tokio::spawn(async { fetch_title(l).await }))
+        .collect();
+    let mut results = vec![];
+    for task in tasks {
+        results.push(task.await.unwrap());
+    }
+
+    for r in results {
+        match r {
+            Some(title) => {
+                let response = format!("↪ {}", title);
+                client.send_privmsg(msg.target, response).unwrap();
+            }
+            None => (),
+        }
+    }
+}
+
+async fn fetch_title(title: String) -> Option<String> {
+    //let response = reqwest::get(title).await.ok()?.text().await.ok()?;
+    //let page = webpage::HTML::from_string(response, None);
+    let page = Webpage::from_url(&title, WebpageOptions::default());
+    match page {
+        Ok(page) => page.html.title,
+        Err(_) => None,
+    }
+}
+
+async fn kick(_client: &Client, _msg: Msg<'_>) {}
+
+async fn invite(_client: &Client, _msg: Msg<'_>) {}
