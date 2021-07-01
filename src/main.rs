c2863639b7d04091d932f27a9fc50d6e87c224e7
Niall Coates <niall.coates@protonmail.com>
2021-07-01 12:19:24 +0100

    (actually) Adds functionality to check when a user was last active

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-use irc::client::prelude::*;
 use futures::prelude::*;
+use irc::client::prelude::*;
 mod bot;
 use bot::process_message;
+mod sqlite;
+use crate::sqlite::Database;
 
 #[tokio::main]
 async fn main() -> Result<(), failure::Error> {
+    let path = "./database.sqlite";
+    let db = Database::open(&path)?;
     let mut client = Client::new("config.toml").await?;
     let mut stream = client.stream()?;
     client.identify()?;
 
-
     while let Some(message) = stream.next().await.transpose()? {
-        process_message(&client, &message).await;
+        process_message(&client, &db, &message).await;
     }
 
     Ok(())
