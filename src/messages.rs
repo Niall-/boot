dd9398cea9f731e0604fac3243b8647e36b4ed80
Niall Coates <1349685+Niall-@users.noreply.github.com>
2021-07-30 01:35:28 +0100

    Handle quitting (i.e., ping timeouts)

--- src/messages.rs
+++ src/messages.rs
@@ -67,6 +67,10 @@ pub async fn process_message(current_nick: &str, message: &Message, tx: mpsc::Se
             ))
             .await
         }
+        Command::QUIT(message) => quit(message, tx.clone()).await,
+        // should handle this at some point but for now I don't care
+        //Command::SAQUIT(nick, message) => saquit(nick, message, tx.clone()).await,
+        //Command::KILL(nick, message) => kill(nick, message, tx.clone()).await,
         _ => (),
     };
 }
@@ -116,3 +120,8 @@ async fn kick(msg: Msg, tx: mpsc::Sender<BotCommand>) {
 }
 
 async fn invite(_msg: Msg) {}
+
+async fn quit(message: &Option<String>, tx: mpsc::Sender<BotCommand>) {
+    println!("QUIT! {:?}", message);
+    tx.send(BotCommand::Quit()).await.unwrap();
+}
